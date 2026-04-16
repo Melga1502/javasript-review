52a3244f34d9cdb060da8c35bf8c90203cd3ac74
alumnos <melgarejojosep@gmail.com>
2026-04-16 16:49:14 -0500

    feat(uuid): add support for generating and validating uuid values.

---
+++ src/shared/domain/model/uuid.js
@@ -0,0 +1,10 @@
+import { v4 as uuidv4, validate as uuidValidate } from 'uuid';
+
+export function generateUuid() {
+    return uuidv4();
+}
+
+export function validateUuid(value){
+    return uuidValidate(value);
+}
+
